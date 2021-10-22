5f6aba8f0e909868b37c5d132bd47eeeb84c3546
h18340637145 <h18340637145@outlook.com>
2021-10-22 13:15:47 +0800

    allocate

---
+++ Allocator/subAllocation/allocator.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#include "alloc.h"
+#include "construct.h"
+
+namespace MiniSTL{
+    //默认二级配置器
+    template <class T, class Alloc = __default_alloc>
+    class simpleAlloc{
+    public: 
+        using value_type = T;
+        using pointer = T*;
+        using const_pointer = const T*;
+        using reference = T&;
+        using const_reference = const T&;
+        using size_type = size_t;
+        using difference_type = ptrdiff_t;
+
+    public:
+    //分配内存空间  释放内存空间
+        static T* allocate();
+        static T* allocate(size_t n);
+        static void deallocate(T* ptr);
+        static void deallocate(T* ptr,size_t n);
+
+        static void construct(T* ptr);
+        static void construct(T* ptr, const T& value);
+        static void destory(T* ptr);
+        static void destory(T* first, T* last);
+    };
+
+    //静态方法
+    template<class T,class Alloc>
+    //无参分配  本质是分配一块大小为当前类型的空间
+    T* simpleAlloc<T,Alloc>::allocate(){
+        return reinterpret_cast<T*>(Alloc::allcate(sizeof(T)));
+    }
+    
+    template<class T,class Alloc>
+    //size n 分配
+    T* simpleAlloc<T,Alloc>::allocate(size_t n){
+        if(n == 0) {
+            return 0;
+        }
+        return reinterpret_cast<T*>(Alloc::allocate(sizeof(T) * n));//分配当前类型*n块的空间
+    }
+
+    //deallocate释放
+    template<class T, class Alloc>
+    void simpleAlloc<T,Alloc>::deallocate(T* ptr){
+        Alloc::deallocate(reinterpret_cast<void*>(ptr),sizeof(T));
+        //释放ptr指向的 一个类型那么大的空间
+    }
+
+    template<class T,class Alloc>
+    void simpleAlloc<T,Alloc>::deallocate(T* ptr,size_t n){
+        //释放ptr指向的  n这么大的空间
+        if(n == 0){
+            return ;
+        }
+        Alloc::deallocate(reinterpret_cast<void*>(ptr),sizeof(T) * n);
+        //释放从ptr指向的地方开始  n*类型那么大的空间
+    }
+
+    //构造
+    template<class T, class Alloc>
+    void simpleAlloc<T,Alloc>::construct(T* ptr){
+        new (ptr) T();// 在ptr指向的位置  调用T的构造函数
+    }
+
+    template<class T,class Alloc>
+    void simpleAlloc<T,Alloc>::construct(T* ptr,const T& value){
+        new (ptr) T(value);
+    }
+
+    //析构
+    template<class T,class Alloc>
+    void simpleAlloc<T,Alloc>::destory(T *ptr){
+        //析构ptr指向的 
+        ptr->~T();
+    }
+
+    template<class T,class Alloc>
+    //析构区间一次调用迭代器指向的对象的析构 first->~T()
+    void simpleAlloc<T,Alloc>::destory(T* first,T * last){
+        for(;first != last;++first){
+            first->~T();
+        }
+    }
+}
---
+++ Allocator/subAllocation/construct.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <new> // placement new 
+#include "typeTrits.h"
+//
+//作用：对已分配的内存进行调用构造函数和析构函数
+    // 调用构造函数使用p指针，和_T1类型的构造函数进行初始化
+    //析构函数  判断_value_type类型是否有  无价值的析构函数   （pod类型都有无价值的 ctor）  
+    // 对于无价值的 -- 不做析构函数调用处理
+namespace MiniSTL{
+    template<class T1,class T2>
+    inline void construct(T1 *p,T2 value){
+        new (p) T1(value);
+    }
+
+    template<class T>
+    inline void destory(T *p){
+        p->~T();
+    }
+
+    //设法利用traits批量析构对象
+    template <class ForwardIterator>
+    inline void destory(ForwardIterator beg,ForwardIterator end){
+        using is_POD_type =  typename _type_traits<ForwardIterator>::is_POD_type;
+        //pod类型的  不用析构
+        _destory_aux(beg,end,is_POD_type());
+    }
+
+    //如果元素的value_type不存在non_trival destructor
+    //一般的类类型  不是pod类型_false_type   需要析构
+    template <class ForwardIterator>
+    inline void _destory_aux(ForwardIterator beg,ForwardIterator end,
+    _false_type){
+        for(; beg != end ; ++beg){
+            destory(&*beg);   //  *beg  迭代器指向的内容  &(*beg) 对迭代器指向的内容取地址
+        }//毕竟迭代器不是真正的地址
+    }
+
+    //存在trival destructor  _true_type   是pod类型 --- 无价值构造---不需要析构---空实现
+    //如果对象的析构函数无关痛痒   那么反复调用它是一种效率上的巨大浪费
+    template<class ForwardIterator>
+    inline void _destory_aux(ForwardIterator,ForwardIterator,_true_type){}
+
+    //针对char* wchar_t* 的特化  字符串？ pod？
+    inline void destory(char * , char * ){}
+    inline void destory(wchar_t*, wchar_t *){}
+}
