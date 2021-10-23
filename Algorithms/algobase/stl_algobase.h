32f5b9002ca96ad9957a22f1ae38d9e2774cd0de
h18340637145 <h18340637145@outlook.com>
2021-10-23 20:23:20 +0800

    mitmatch

--- Algorithms/algobase/stl_algobase.h
+++ Algorithms/algobase/stl_algobase.h
@@ -5,3 +5,136 @@
 #include "stl_function.h"
 #include "stl_iterator.h"
 #include "typeTraits.h"
+
+
+namespace MiniSTL
+{
+    //不能保证长度相等
+    template<class InputIterator1 , class InputIterator2>
+    inline bool equal(InputIterator1 first1,InputIterator1 last1,
+                        InputIterator2 first2){
+        for(;first1 != last1;++first1,++first2){//第一个容器的遍历
+            if(*first1 != *first2){
+                return false;
+            }
+        }
+        //仅仅是在第一个容器范围内比较，如果第二容器比第一个容器大，则比较不能保证
+        return true;
+    }
+    
+    //接受自定义比较器
+    template<class InputIterator1, class InputItrator2,class BinaryPredicate>
+    inline bool equal(InputIterator1 first1,InputIterator1 last1,
+                        InputItrator2 first2,BinaryPredicate binary_pred){
+        for(;first1 != last1;first1++,first2++){
+            if(!(*first1,*first2)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //填充，将[)区间使用value填充
+    template<class ForwardIterator,class T>
+    void fill(ForwardIterator first, ForwardIterator last, const T& value){
+        for(; first != last ; ++first){
+            *first = value;
+        }
+    }
+
+    //显然，本算法执行覆写操作，因此通常配合inserter完成
+    //从first开始  n个数据都赋值为value，返回结尾处
+    template<class OutputIterator, class Size,class T>
+    OutputIterator fill_n(OutputIterator first,Size n,const T &value){
+        for(;n > 0;--n,++first){
+            *first = value;
+        }
+        return first;
+    }
+
+    template<class ForwardIterator1,class ForwardIterator2>
+    inline void iter_swap(ForwardIterator1 a,ForwardIterator2 b){
+        return __iter_swap(a,b,value_type_t<ForwardIterator1>());
+    }
+
+    //  直到迭代器指向对象的类型，才能构造对象，因此本初使用了value_type
+    template<class ForwardIterator1,class ForwardIterator2,class T>
+    inline void iter_swap(ForwardIterator1 a,ForwardIterator2 b,T){
+        T temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+
+    template<class InputIterator1,class InputIterator2>
+    bool lexicographical_compare(InputIterator1 first1,InputIterator1 last1,
+                                InputIterator2 first2, InputIterator2 last2){
+        for(;first1 != last1 && first2 != last2;++first1,++first2){
+            if(*first1 < *first2){
+                return true;
+            }else if(*first1 > *first2){
+                return false;
+            }
+        }
+        return first1 == last1 &&  //1到头  2没到头  ---->2有剩余 返回true(说明1小2长)
+                    first2 != last2
+    }
+
+    template<class T>
+    inline const T &max(const T&a, const T& b){
+        return a < b ? b :a;//返回大值 引用记录
+    }
+
+    template<class T,class Compare>
+    inline const T &max(const T& a , const T &b, Compare comp){
+        return comp(a,b) ? b : a;
+    }
+
+    template<class T>
+    inline const T& min(const T& a, const T& b){
+        return b < a ? b: a;
+    }
+
+    template<class T,class Compare>
+    inline const T& min(const T& a , const T& b,Compare comp){
+        return comp(b ,a) ? b:a;
+    }
+
+    //词典序比较
+    template<class InputIterator1,class InputIterator2,class Compare>
+    bool lexicographical_compare(InputIterator1 first1,InputIterator1 last1,
+                                InputIterator2 first2, InputIterator2 last2,
+                                Compare comp){
+        for(;first1 != last1 && first2!=last2;first1++,first2++){
+            if(comp(*first1,*first2)){
+                return true;
+            }else if(comp(*first2,*first1)){
+                return false;
+            }
+        }
+        return first1 == last1 && 
+                    first2 != last2;    //二序列有余  二长 返回true  比较大小默认<
+    }
+
+    //针对原始指针const unsigned char * 的全特化版本
+    //字符串版本的比较
+    inline bool lexicographical_compare(const unsigned char * first1,
+                                        const unsigned char * last1,
+                                        const unsigned char * first2,
+                                        const unsigned char * last2){
+        const size_t len1 = last1 - first1;
+        const size_t len2 = last2 - first2;
+        //先比较长度相同的段落
+        const int result = memcmp(first1,first2,min(len1,len2));//依次比较
+        //如果结果==0 ，说明前面相同长度出，每个字符都相同
+            //只能比较长度了--->默认长度小的
+        //如果结果!=0，如果小的个数多则为负值，所以返回result<0
+        return result != 0 ? result < 0 :len1 < len2;
+    }
+
+    //显然要求序列1长于序列2
+    // mismathch 找到两个容器中，第一个不匹配所出现的位置 it1 和it2
+    
+    
+
+
+} // namespace MiniSTL
