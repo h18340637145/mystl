66b86ab782564525b05616574ed7626223306ac2
h18340637145 <h18340637145@outlook.com>
2021-10-24 16:34:51 +0800

    uninitialized.j

---
+++ Allocator/subAllocation/uninitialized.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <cstring>
+#include "construct.h"
+#include "stl_algobase.h"
+#include "stl_iterator.h"
+#include "typeTraits.h"
+
+namespace MiniSTL
+{
+    template<class InputIterator, class ForwardIterator>
+    inline ForwardIterator uninitialized_copy(InputIterator first,
+                                                InputIterator last,
+                                                ForwardIterator result){
+    
+    }
+} // namespace MiniSTL
