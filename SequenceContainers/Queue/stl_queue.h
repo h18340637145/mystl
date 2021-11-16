13e8aa6d46cdea35311b4ce4c33376ce36c82a3b
h18340637145 <h18340637145@outlook.com>
2021-11-16 13:48:21 +0800

    push

--- SequenceContainers/Queue/stl_queue.h
+++ SequenceContainers/Queue/stl_queue.h
@@ -14,7 +14,7 @@ namespace MiniSTL
     template<class T, class Sequence>
     bool operator==(const queue<T, Sequence> & , const queue<T,Sequence>&);
     template<class T, class Sequence>
-    bool operator<(const queue<T, Sequence> & ,const queue<T,Sequece> &);
+    bool operator<(const queue<T, Sequence> & ,const queue<T,Sequence> &);
 
     template<class T, class Sequence>
     class queue{
@@ -53,5 +53,67 @@ namespace MiniSTL
             c.operator=(std::move(rhs.c));
             return *this;
         }
+
+    public:
+        bool empty() const noexcept {
+            return c.empty();
+        }
+        size_type size() const noexcept {
+            return c.size();
+        }
+
+        const_reference front() const{
+            return c.front();
+        }
+
+    public:
+        //settter
+        reference front(){
+            return c.front();
+        }
+    public:
+        //push pop
+        void push(const value_type & value){
+            c.psuh_back(value);
+        }
+
+        void pop(){
+            c.pop_front();
+        }
+    };
+
+    template<class T, class Sequence>
+    inline bool operator==(const queue<T,Sequence> &lhs, const queue<T, Sequence> &rhs){
+        return lhs.c == rhs.c;
+    }
+    template<class T, class Sequence>
+    inline bool operator<(const queue<T,Sequence> &lhs, const queue<T,Sequence> &rhs){
+        return lhs.c < rhs.c;
+    }
+
+    template<class T, class Sequence>
+    inline bool operator!= (const queue<T,Sequence> &lhs, const queue<T,Sequence> &rhs){
+        return !(lhs ==rhs);
+    }
+
+    template<class T, class Sequence>
+    inline bool operator>(const queue<T, Sequence> &lhs, const queue<T,Sequence> &rhs){
+        return rhs.c < lhs.c;
+    }
+
+    template<class T, class Sequence>
+    inline bool operator<=(const queue<T, Sequence> &lhs, const queue<T,Sequence> &rhs){
+        return !(rhs < lhs);
+
+    }
+
+    template<class T, class Sequence>
+    inline bool operator>=(const queue<T,Sequence> &lhs, const queue<T,Sequence> &rhs){
+        return !(lhs < rhs);
+    }
+
+    template<class T, class Sequence>
+    inline void swap(queue<T, Sequence> &lhs, queue<T,Sequence> &rhs)noexcept{
+        lhs.swap(rhs);
     }
 } // namespace MiniSTL
